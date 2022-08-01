0512b0df3e85a04821ba426711a531f778290058
SURAJ SINGH RANA <100834595+surajrana45@users.noreply.github.com>
2022-08-01 14:55:52 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+
+import React from "react";
+import reactDom from "react-dom";
+
+reactDom.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodle</li>
+    </ul>
+  </div>,
+  document.querySelector("#root")
+);
